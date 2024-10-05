37de58bc14629ba95c5dad5118561beadd76d876
Nuzair46 <nuzer501@gmail.com>
2024-10-05 12:32:41 +0900

    improved tests

--- test/test_rails_erb_credentials.rb
+++ test/test_rails_erb_credentials.rb
@@ -5,7 +5,8 @@ require "dummy/config/environment"
 
 class TestRailsErbCredentials < Minitest::Test
   def setup
-    
+    ENV['TEST'] = "my_actual_secret_for_test"
+    ENV['TEST3'] = "my_actual_secret_for_test3"
   end
 
   def test_that_it_has_a_version_number
@@ -13,13 +14,12 @@ class TestRailsErbCredentials < Minitest::Test
   end
 
   def test_evaluates_erb_in_credentials
-    ENV['TEST'] = "my_actual_secret"
+    # <%= ENV["TEST"] %>
     assert_equal "my_actual_secret", Rails.application.credentials.test
   end
 
   def test_evaluates_erb_in_credentials_when_it_has_multiple_env_vars
     # <%= ENV["TEST3"] || test3 %>
-    ENV['TEST3'] = "my_actual_secret"
-    assert_equal "my_actual_secret", Rails.application.credentials.test3
+    assert_equal "my_actual_secret_for_test3", Rails.application.credentials.test3
   end
 end
